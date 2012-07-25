eb4fbfa63ac40629940dd21b6766b90273bde07d
Carlos Eduardo C. Silva <kadu@kadu.com.br>
2012-07-25 06:18:12 -0300

    Bomba

--- bomba_02b/bomba_02b.ino
+++ bomba_02b/bomba_02b.ino
@@ -14,6 +14,8 @@
 // Auxiliares 
 #define APERTADO 1
 #define NAO_APERTADO 0
+#define iSTART 0
+#define iCOUNT 1
 
 // Configuracoes padroes
 #define TEMPO_PADRAO_BOMBA 2
@@ -45,13 +47,20 @@
 	int tempoBombaSetup = TEMPO_PADRAO_BOMBA;
 	
   // Sessions
-	int setup_session = 0;
+	int setup_session        = 0;
+	int rb_state_time[2]     = {0,0};
+	int pb_state_time[2]     = {0,0};
+	int mb_state_time[2]     = {0,0};
+	int disarm_state_time[2] = {0,0};
+	int left_timmers[3] = {0,0,0};
 	
 	// Helpers
 	long debounceDelay = 50;
 	int reading[3];
 	int defuse_button_rnd;
 	int botao_cancela_estouro;
+	
+	int flagw = 0;
 
 //** Fim Variaveis
 
@@ -71,11 +80,38 @@ void setup() {
 	pinMode(ledPin, OUTPUT);  
 
 	// timed actions setup  
-	timer.setInterval(1000, count_secs);
+	timer.setInterval(1000 , count_secs);
 	timer.setInterval(60000, StatusDislpay); // 1 por minuto
+	left_timmers[0] = timer.setInterval(60000, left_5mins);
+	left_timmers[1] = timer.setInterval(10000, left_1min);
+	left_timmers[2] = timer.setInterval(1000 , left_15secs);
+	for(size_t i = 0; i < 3; i++)
+	{
+		timer.disable(left_timmers[i]);
+	}
 	configura_botao_de_desarmar();
 
 	Serial.println("Setup OK");
+	Serial.println("Waiting...");
+}
+
+
+void left_5mins() {
+		digitalWrite(ledPin,HIGH);
+		delay(300);
+		digitalWrite(ledPin,LOW);	
+}
+
+void left_1min() {
+	digitalWrite(ledPin,HIGH);
+	delay(300);
+	digitalWrite(ledPin,LOW);
+}
+
+void left_15secs() {
+	digitalWrite(ledPin,HIGH);
+	delay(300);
+	digitalWrite(ledPin,LOW);	
 }
 
 
@@ -84,8 +120,28 @@ void setup() {
  */
 void count_secs() {
   secs += 1;
-	Serial.print("Secs = "); Serial.print(secs); 
-	Serial.print("  Tempo bomba = "); Serial.println(tempoBomba);
+  int countdown_time = tempoBomba - secs;
+
+	if(countdown_time < 15) {
+		timer.disable(left_timmers[1]);
+		if(!timer.isEnabled(left_timmers[2])) {
+			timer.enable(left_timmers[2]);
+		}
+	} else {
+		if(countdown_time < 60) {
+			timer.disable(left_timmers[0]);
+			if(!timer.isEnabled(left_timmers[1])) {
+				timer.enable(left_timmers[1]);
+			}
+		} else {
+			if(countdown_time < 300) {
+				if(!timer.isEnabled(left_timmers[0])) {
+					timer.enable(left_timmers[0]);
+				}
+			}	
+		}
+	}
+
 }
 
 
@@ -95,17 +151,18 @@ void count_secs() {
 void StatusDislpay() {
 	int countdown_time = tempoBomba - secs;
 	Serial.print("* Status - Restam ");
-	if(countdown_time < 60)
-	{
-		Serial.print(countdown_time);
-		Serial.println(" segundos");
-	} else {
-		countdown_time = countdown_time / 60;
-		Serial.print(countdown_time);
-		Serial.println(" minutos");
+	if(countdown_time > 0) {
+		if(countdown_time < 60)
+		{
+			Serial.print(countdown_time);
+			Serial.println(" segundos");
+		} else {
+			countdown_time = countdown_time / 60;
+			Serial.print(countdown_time);
+			Serial.println(" minutos");
+		}
+		Serial.println("");
 	}
-	
-	Serial.println("");
 }
 
 /**
@@ -203,30 +260,31 @@ void goto_setup() {
 			tempoBombaSetup--;
 
 		// Açoes iguais em todo aberto de botao
-		if((buttonState[PB] == APERTADO) || (buttonState[MB] == APERTADO) || (buttonState[RB] == APERTADO)) {
+		if((buttonState[PB] == APERTADO) || (buttonState[MB] == APERTADO)) {
 			zera_variaveis();
 			setup_session = secs;
 			Serial.print("Setup : ");
 			Serial.println(tempoBombaSetup);
 		}
-		
-		// Reset
-    if(buttonState[RB] == APERTADO) {
-      setup_session = secs - 15; 
-			break;     
-    }
 
-		// Controle da sessao
-		if(secs > setup_session + SETUP_SESSION_TIMEOUT) {
-			tempoBomba = secs + tempoBombaSetup * 60 ;
-			
-			Serial.print("Bomb will fired on ");
-			Serial.print(tempoBombaSetup);
-      Serial.println(" mins");      
-			zera_variaveis();
-      break;
-		} 
-
+		// Reset Button
+		if(buttonState[RB] == APERTADO) {
+			if(rb_state_time[iSTART] == 0) {
+				rb_state_time[iSTART] = secs;
+				Serial.println("RBF");
+			}
+			rb_state_time[iCOUNT] = secs;
+			if(rb_state_time[iSTART] < secs - 4) {
+				Serial.println("RESET Button FIRED");
+				zera_variaveis();
+				setup_session = secs;
+				Serial.print("Setup : ");
+				Serial.println(tempoBombaSetup);
+				break;
+			}
+		} else {
+			rb_state_time[iSTART] = 0;
+		}
 	}
 	aguarda_entre_sessoes();
 }
@@ -240,6 +298,7 @@ void aguarda_entre_sessoes() {
 		delay(300);
 	}
 	delay(300);
+	zera_variaveis();
 }
 
 /**
@@ -250,6 +309,52 @@ void goto_limbo() {
 	delay(5000); // 3 secs
 	Serial.println("Exit limbo...");
 }
+/**
+ * Function to control armed status
+ */
+void goto_armed() {
+	zera_variaveis();
+	configura_botao_de_desarmar();
+	tempoBomba = secs + tempoBombaSetup * 60 ;
+
+  Serial.println("Entering in ARMED mode");
+
+	// Fica no while de configuração até algumas condições de parada ser executado
+	while(1) {
+		timer.run();
+		ctrlButtons();
+		
+		// Disarm button
+		if(buttonState[botao_cancela_estouro ? PB : MB] == APERTADO) {
+			if(disarm_state_time[iSTART] == 0) {
+				disarm_state_time[iSTART] = secs;
+				Serial.println("DISARM PRESSED");
+			}
+			disarm_state_time[iCOUNT] = secs;
+			if(disarm_state_time[iSTART] < secs - 4) {
+				Serial.println("DISARMED");
+				break;
+			}
+		} else {
+			disarm_state_time[iSTART] = 0;
+		}
+
+		if(buttonState[!botao_cancela_estouro ? PB : MB] == APERTADO) {
+			Serial.println("FIRE IN THE HOLE!!!");
+			dispara();
+			flagw = !flagw;
+			break;
+		}
+		
+		if(secs > tempoBomba) {
+	    Serial.println("Time is UP");
+	    dispara();
+			flagw = !flagw;
+			break;
+		}
+	}
+	aguarda_entre_sessoes();
+}
 
 /**
  * Loop
@@ -257,35 +362,57 @@ void goto_limbo() {
 void loop() {
 	timer.run();
 	ctrlButtons();
-	
-	if(buttonState[RB] == APERTADO) {
-    Serial.println("RESET Button FIRED");
-		dispara();
-		goto_setup();
+	if(flagw) {
+		Serial.println("Waiting...");
+		flagw = !flagw;
 	}
-
-  if(buttonState[PB] == APERTADO) {
-    Serial.println("Plus Button FIRED");
-    if (botao_cancela_estouro) {
-      goto_limbo();
-    } else {
-      secs = tempoBomba;
-    }
-  }
-  
-  if(buttonState[MB] == APERTADO) {
-    Serial.println("Minus Button FIRED");
-    if (!botao_cancela_estouro) {
-      goto_limbo();
-    } else {
-      secs = tempoBomba;
-    }
-  }
 	
-		
-	if(secs > tempoBomba) {
-    Serial.println("Time is UP");
-    dispara();
-		goto_limbo();
+	// Reset Button
+	if(buttonState[RB] == APERTADO) {
+		if(rb_state_time[iSTART] == 0) {
+			rb_state_time[iSTART] = secs;
+			Serial.println("RBF");
+		}
+		rb_state_time[iCOUNT] = secs;
+		if(rb_state_time[iSTART] < secs - 4) {
+			Serial.println("RESET Button FIRED");
+			dispara();
+			aguarda_entre_sessoes();
+			goto_setup();	
+		}
+	} else {
+		rb_state_time[iSTART] = 0;
+	}
+	
+	// Plus Button
+	if(buttonState[PB] == APERTADO) {
+		if(pb_state_time[iSTART] == 0) {
+			pb_state_time[iSTART] = secs;
+			Serial.println("PBF");
+		}
+		pb_state_time[iCOUNT] = secs;
+		if(pb_state_time[iSTART] < secs - 4) {
+			Serial.println("PLUS Button FIRED");
+			aguarda_entre_sessoes();
+			goto_armed();
+		}
+	} else {
+		pb_state_time[iSTART] = 0;
+	}	
+	
+	// Minus Button
+	if(buttonState[MB] == APERTADO) {
+		if(mb_state_time[iSTART] == 0) {
+			mb_state_time[iSTART] = secs;
+			Serial.println("MBF");
+		}
+		mb_state_time[iCOUNT] = secs;
+		if(mb_state_time[iSTART] < secs - 4) {
+			Serial.println("Minus Button FIRED");
+			aguarda_entre_sessoes();
+			goto_armed();
+		}
+	} else {
+		mb_state_time[iSTART] = 0;
 	}
 }
